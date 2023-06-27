45f225a6d548324fd462560da6be6f273b23aa56
mubarak <mubarakcali24@gmail.com>
2023-06-27 11:41:34 -0700

    reactionroutes

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/thoughts', require('./routes/thoughtRoutes'));
+app.use('/api/thoughts', require('./routes/reactionRoutes'));
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
