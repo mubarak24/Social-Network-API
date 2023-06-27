83cb9088834fba9f2e04c52469d0ef3cd53d49c2
mubarak <mubarakcali24@gmail.com>
2023-06-27 16:45:35 -0700

    routes fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -65,4 +65,42 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+// POST add a friend to a user
+router.post('/:userId/friends', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const friendId = req.body.friendId;
+    if (user.friends.includes(friendId)) {
+      return res.status(400).json({ error: 'Friend already added' });
+    }
+    user.friends.push(friendId);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to add friend' });
+  }
+});
+
+// DELETE remove a friend from a user
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const friendId = req.params.friendId;
+    if (!user.friends.includes(friendId)) {
+      return res.status(400).json({ error: 'Friend not found' });
+    }
+    user.friends.pull(friendId);
+    await user.save();
+    res.json({ message: 'Friend removed successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to remove friend' });
+  }
+});
+
 module.exports = router;
