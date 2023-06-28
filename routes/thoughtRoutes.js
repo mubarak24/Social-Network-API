ed526e079dc2c7906344e225b6bd01d09746bd5a
mubarak <mubarakcali24@gmail.com>
2023-06-27 17:50:49 -0700

    edit routes for insomnia

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -66,31 +66,38 @@ router.delete('/:id', async (req, res) => {
 // POST create a reaction for a thought
 router.post('/:thoughtId/reactions', async (req, res) => {
   try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ error: 'Thought not found' });
+      const thought  = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      if (!thought ) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+      res.json(thought );
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-    thought.reactions.push(req.body);
-    await thought.save();
-    res.status(201).json(thought);
-  } catch (error) {
-    res.status(400).json({ error: 'Failed to create reaction' });
-  }
 });
 
 // DELETE remove a reaction from a thought
 router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
   try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ error: 'Thought not found' });
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-    thought.reactions.pull({ _id: req.params.reactionId });
-    await thought.save();
-    res.json({ message: 'Reaction deleted successfully' });
-  } catch (error) {
-    res.status(400).json({ error: 'Failed to delete reaction' });
-  }
-});
+  },
+);
 
 module.exports = router;
